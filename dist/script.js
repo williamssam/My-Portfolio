db18e49f5fef44bd831af71075b62a83728bf2b7
Williams Samuel <samwill300@gmail.com>
2021-12-23 02:05:47 +0100

    correct some typographical errors and added new project

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,11 @@
 const date = document.getElementById('date')
 
-const setCurrentDate = () => {
+const setCurrentYear = () => {
 	const currentYear = new Date().getFullYear()
 	date.textContent = currentYear
 }
 
-setCurrentDate()
+setCurrentYear()
 
 // hamburger menu
 const hamburger = document.querySelector('.hamburger')
@@ -13,15 +13,15 @@ const navList = document.querySelector('.nav-list')
 const socials = document.querySelector('.social-nav')
 const navLinks = document.querySelectorAll('.nav-links')
 
-const openMobileMenu = () => {
+const toggleMobileMenu = () => {
 	hamburger.classList.toggle('open')
 	navList.classList.toggle('open')
 	socials.classList.toggle('open')
 	document.body.classList.toggle('open')
 }
 
-navLinks.forEach((link) => link.addEventListener('click', openMobileMenu))
-hamburger.addEventListener('click', openMobileMenu)
+navLinks.forEach((link) => link.addEventListener('click', toggleMobileMenu))
+hamburger.addEventListener('click', toggleMobileMenu)
 
 // initialize aos (library for scroll animation)
 AOS.init()
